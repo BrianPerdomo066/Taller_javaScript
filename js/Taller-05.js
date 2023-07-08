e4e47af5fca8e9916bfb3a9e5ef3b9e12dee3b5c
brian perdomo <brayanperdomovelasquez@gmail.com>
2023-07-07 19:10:30 -0500

    [+]agrege el taller-(05,06)

---
+++ js/Taller-05.js
@@ -0,0 +1,37 @@
+// Se le dan dos matrices ordenadas que solo contienen números enteros. 
+// Su tarea es encontrar una manera de fusionarlos en uno solo, ordenados
+//  en orden ascendente. Complete la función combinarListas( lista1, lista2 ), 
+// donde lista1 y lista2 son las listas ordenadas originales.
+
+// No necesita preocuparse por la validación, ya que arr1 y arr2 deben
+//  ser matrices con 0 o más enteros. Si tanto arr1 como arr2 están vacíos,
+//   simplemente devuelva una lista vacía.
+
+// Nota: lista1 y lista2 pueden clasificarse en diferentes órdenes. 
+// También lista1 y lista2 pueden tener los mismos números enteros. 
+// Eliminar duplicado en el resultado devuelto.
+
+// Resultado esperado
+
+const lista1 = [1, 3, 5, 7, 9, 11, 12]                  //[1, 3, 5, 7, 9]                       //1[1, 2, 3, 4, 5]
+
+const lista2 = [1, 2, 3, 4, 5, 10, 12]                  //[10, 8, 6, 4, 2]                       //2[6, 7, 8, 9, 10]
+
+const lista3 = lista1.concat(lista2);
+
+for(let i = 0; i < lista3.length -1; i++ ) {
+    for(let j = 0; j < lista3.length -1 -i; j++){
+        if (lista3[j] > lista3[j + 1]){
+
+            temp = lista3[j];
+            lista3[j] = lista3[j + 1];
+            lista3[j + 1] = temp;
+            
+        }
+    }
+}
+
+console.log( lista3 );
+
+
+
---
+++ js/Taller-06.js
@@ -0,0 +1,8 @@
+// Hubo un examen en tu clase y lo pasaste. ¡Felicidades! Pero eres una persona ambiciosa. Quiere saber si es mejor que el estudiante promedio de su clase.
+
+// Recibe una lista con los puntajes de las pruebas de sus compañeros. ¡Ahora calcule el promedio y compare su puntaje!
+
+// ¡Devuelve True si estás mejor, de lo contrario False!
+
+// Nota: Sus puntos no están incluidos en la lista de puntos de su clase. ¡Para calcular el punto promedio, puede agregar su punto a la lista dada!
+
